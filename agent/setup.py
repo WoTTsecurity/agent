bf3a9303de37f68245a5d845a2e4a80a68a56193
Viktor Petersson <vpetersson@wireload.net>
2019-01-05 18:04:58 +0000

    [WIP] Build .deb packages

---
+++ agent/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="wott-agent",
+    version='0.1.0',
+
+    author="Viktor Petersson",
+    author_email="v@viktopia.io",
+
+    description="WoTT agent",
+
+    packages=setuptools.find_packages('src', exclude=('tests',)),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+
+    install_requires=required,
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'wott-agent = agent:main',
+        ],
+    }
+)

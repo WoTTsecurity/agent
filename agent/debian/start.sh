d72382ff32f7f34b92a2a8e4b0d539687ec49670
Viktor Petersson <vpetersson@wireload.net>
2019-01-09 16:51:00 +0000

    In-between commit to test on the Pi

--- agent/debian/start.sh
+++ agent/debian/start.sh
@@ -9,8 +9,23 @@ if [ -e ./debian/dist/*.deb ]; then \
     rm -f ./debian/dist/*.deb ./debian/dist/*.changes; \
 fi;
 
+
 dpkg-buildpackage -B -us -uc --changes-option=-udebian/dist/
+
 
 # Copy out the files
 cp -v /tmp/python-deb-pkg/debian/dist/*.deb /dist/
 cp -v ../*.changes /dist/
+
+
+## The section below "works" for building a source package.
+
+#export DEBUILD_DPKG_BUILDPACKAGE_OPTS="-us -uc -I -i --changes-option=-udebian/dist/"
+#export DEBUILD_LINTIAN_OPTS="-i -I --show-overrides"
+
+#debuild -S -us -uc
+
+#dpkg-buildpackage -us -uc --changes-option=-udebian/dist/
+
+# Copy out the files
+#cp -v /tmp/wott-agent* /dist/

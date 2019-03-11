f2fdccaa9090bf75259426e364a7718e46b5d889
Sergey Lilo <power.real@gmail.com>
2019-03-11 23:01:19 +0600

    Add is_firewall_enabled() check.

--- agent/security_helper.py
+++ agent/security_helper.py
@@ -1,3 +1,5 @@
+from unittest import mock
+import iptc
 from xml.etree import ElementTree as ET
 from sh import nmap
 
@@ -25,3 +27,28 @@ def nmap_scan(target):
                 'state': state
             })
     return result
+
+
+def test_firewall_enabled_pos():
+    with mock.patch('iptc.Table') as ipt:
+        chain0 = mock.Mock()
+        chain0.name = 'INPUT'
+        chain0.rules = [object(), object()]
+        ipt.return_value = [chain0]
+        assert is_firewall_enabled() is True
+
+
+def test_firewall_enabled_neg():
+    with mock.patch('iptc.Table') as ipt:
+        chain0 = mock.Mock()
+        chain0.name = 'INPUT'
+        chain0.rules = []
+        ipt.return_value = [chain0]
+        assert is_firewall_enabled() is False
+
+
+def is_firewall_enabled():
+    """Check if FILTER INPUT chain contains any rule"""
+    filter_table = iptc.Table(iptc.Table.FILTER)
+    input_chain = next(filter(lambda c: c.name == 'INPUT', filter_table))
+    return not len(input_chain.rules) == 0

ed3a19077c1d1f96c4e30c1ae56b623385af7476
Artem Martynovich <artem.martynovich@gmail.com>
2019-05-14 22:40:35 +0600

    Add missing dependency on python-systemd. Bump Python version. deb: 0.1.4.6

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wott-agent",
-    version='0.1.2',
+    version='0.1.4',
 
     author="Viktor Petersson",
     author_email="v@viktopia.io",

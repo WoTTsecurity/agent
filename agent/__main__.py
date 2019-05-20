4f45d5ab26f7f6a4905323134cd9b01b22895787
Artem Martynovich <artem.martynovich@gmail.com>
2019-05-21 01:24:47 +0600

    Add periodic process runner. deb: 0.1.5

--- agent/__main__.py
+++ agent/__main__.py
@@ -1,6 +1,6 @@
 import argparse
 from . import run, get_device_id, get_open_ports, say_hello,\
-    get_claim_token, get_claim_url
+    get_claim_token, get_claim_url, executor
 
 
 def main():
@@ -9,7 +9,8 @@ def main():
         'portscan': (get_open_ports, "Print open ports."),
         'test-cert': (say_hello, "Validate device certificate."),
         'claim-token': (get_claim_token, "Print claim token."),
-        'claim-url': (get_claim_url, "Print claim URL.")
+        'claim-url': (get_claim_url, "Print claim URL."),
+        'daemon': (run_daemon, "Run as daemon")
     }
     help_string = "One of the following:\n\n" + "\n".join(
         ["{: <12} {: <}".format(k, v[1]) for k, v in actions.items()])
@@ -37,11 +38,25 @@ or renews it if necessary.
         action="store_true",
         help="Developer mode: work with locally running server.")
     args = parser.parse_args()
+
     if not args.action:
         run(ping=True, debug=args.debug, dev=args.dev)
+    elif args.action == 'daemon':
+        run_daemon(debug=args.debug, dev=args.dev)
     else:
         run(ping=False, debug=args.debug, dev=args.dev)
         print(actions[args.action][0]())
+
+
+PING_INTERVAL = 60*60
+PING_TIMEOUT = 10*60
+
+
+def run_daemon(debug, dev):
+    exe = executor.Executor(PING_INTERVAL, run, (True, debug, dev),
+                            timeout=PING_TIMEOUT)
+    executor.schedule(exe)
+    executor.spin()
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wott-agent",
-    version='0.1.4',
+    version='0.1.5',
 
     author="Viktor Petersson",
     author_email="v@viktopia.io",

f1001878860876e9593ab16b44b5ef3a824ce039
eugenosm <emazitov@gmail.com>
2019-06-21 15:44:56 +0600

    change owner to linux_user in credentials

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -501,10 +501,10 @@ def test_fetch_credentials(tmpdir):
     mock_resp.raise_status = 200
     mock_resp.json = mock.Mock(
         return_value=[
-            {'name': 'name1', 'key': 'key1', 'value': 'v1', 'owner': user},
-            {'name': 'name2', 'key': 'key1', 'value': 'v21', 'owner': user},
-            {'name': 'name2', 'key': 'key2', 'value': 'v22', 'owner': user},
-            {'name': 'name2', 'key': 'key3', 'value': 'v23', 'owner': ''},
+            {'name': 'name1', 'key': 'key1', 'value': 'v1', 'linux_user': user},
+            {'name': 'name2', 'key': 'key1', 'value': 'v21', 'linux_user': user},
+            {'name': 'name2', 'key': 'key2', 'value': 'v22', 'linux_user': user},
+            {'name': 'name2', 'key': 'key3', 'value': 'v23', 'linux_user': ''},
         ]
     )
     mock_resp.return_value.ok = True

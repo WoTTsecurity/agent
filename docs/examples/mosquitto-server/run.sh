203576f4d8454479fd04534694b506e48f05114c
Fiona McAllister <fshmcallister@gmail.com>
2019-07-16 09:53:25 +0100

    moved mosquitto mqtt

--- docs/examples/mosquitto-server/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-    -p 8883:8883 \
-    -v /opt/wott/certs:/opt/wott/certs:ro \
-    -v $(pwd)/mosquitto.conf:/mosquitto/config/mosquitto.conf \
-    -v $(pwd)/permission.acl:/mosquitto/config/permission.acl \
-    eclipse-mosquitto

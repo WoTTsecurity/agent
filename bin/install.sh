def655b69dc5f8d27b5d340c270f1a25b5fed9ec
Viktor Petersson <self@vpetersson.com>
2019-12-20 13:02:06 +0000

    Refactors distribution detection

--- bin/install.sh
+++ bin/install.sh
@@ -4,6 +4,12 @@
 
 set -euo pipefail
 
+# Installation method for Debian derived systems
+function install_on_debian_based {
+    curl -s https://packagecloud.io/install/repositories/wott/agent/script.deb.sh | bash
+    apt install -y wott-agent
+}
+
 # Wrap everything in a function to ensure
 # that we don't get a partial run.
 function main {
@@ -18,17 +24,27 @@ function main {
         echo -e "[DEFAULT]\\nenroll_token = $CLAIM_TOKEN" > /opt/wott/config.ini
     fi
 
-    # If Ubuntu/Debian is detected
+    # This should for all recent versions of Debian and Ubuntu
+    # as well as CentOS 7 or later.
     if [ -f /etc/os-release ]; then
-        # Install repository from Package Cloud
-        curl -s https://packagecloud.io/install/repositories/wott/agent/script.deb.sh | bash
-        apt install -y wott-agent
+        DISTRO=$(grep ^ID= /etc/os-release | sed 's/ID=//g')
+    else
+        echo "Unable to detect distribution. Exiting."
+        exit 1
     fi
 
-    # If RHEL/CentOS/Amazon Linux is detected (for when we add support)
-    #if [ -f /etc/redhat-release ]; then
-    #    yum update
-    #fi
+    # If Ubuntu/Debian is detected
+    if [ "$DISTRO" = "debian" ]; then
+        install_on_debian_based
+    elif [ "$DISTRO" = "ubuntu" ]; then
+        install_on_debian_based
+    elif [ "$DISTRO" = "fedora" ]; then
+        echo "Detected Fedora. This is currently an unsupported distribution."
+        exit 1
+    elif [ "$DISTRO" = '"centos"' ]; then
+        echo "Detected CentOS. This is currently an unsupported distribution."
+        exit 1
+    fi
 }
 
 main

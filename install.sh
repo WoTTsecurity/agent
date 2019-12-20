f7553ba7df26c9baa04afb7b41d9661586a27673
Viktor Petersson <self@vpetersson.com>
2019-12-20 12:30:45 +0000

    Fixes feedack

--- install.sh
+++ install.sh
@@ -4,25 +4,31 @@
 
 set -euo pipefail
 
-if [[ $(id -u) -ne 0 ]]; then
-    echo "Please run as root or using sudo"
-    exit 1
-fi
+# Wrap everything in a function to ensure
+# that we don't get a partial run.
+function main {
+    if [[ $(id -u) -ne 0 ]]; then
+        echo "Please run as root or using sudo"
+        exit 1
+    fi
 
-mkdir -p /opt/wott
+    mkdir -p /opt/wott
 
-if [ ! -f /etc/wott/config.ini ]; then
-    echo -e "[DEFAULT]\\nenroll_token = $CLAIM_TOKEN" > /opt/wott/config.ini
-fi
+    if [ ! -f /opt/wott/config.ini ]; then
+        echo -e "[DEFAULT]\\nenroll_token = $CLAIM_TOKEN" > /opt/wott/config.ini
+    fi
 
-# If Ubuntu/Debian is detected
-if [ -f /etc/lsb-release ]; then
-    # Install repository from Package Cloud
-    curl -s https://packagecloud.io/install/repositories/wott/agent/script.deb.sh | bash
-    apt install -y wott-agent
-fi
+    # If Ubuntu/Debian is detected
+    if [ -f /etc/os-release ]; then
+        # Install repository from Package Cloud
+        curl -s https://packagecloud.io/install/repositories/wott/agent/script.deb.sh | bash
+        apt install -y wott-agent
+    fi
 
-# If RHEL/CentOS/Amazon Linux is detected (for when we add support)
-#if [ -f /etc/redhat-release ]; then
-#    yum update
-#fi
+    # If RHEL/CentOS/Amazon Linux is detected (for when we add support)
+    #if [ -f /etc/redhat-release ]; then
+    #    yum update
+    #fi
+}
+
+main

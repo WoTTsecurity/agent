899efa53e74f410aeb337c65a0165d6dd338dbf0
Artem Martynovich <artem.martynovich@gmail.com>
2020-03-18 22:32:29 +0600

    Properly install wott-agent services on amzn.

---
+++ bin/wott-agent-remove.sh
@@ -0,0 +1,6 @@
+systemctl stop wott-agent.service
+systemctl disable wott-agent.service
+systemctl stop wott-agent-self-update.timer
+systemctl disable wott-agent-self-update.timer
+rm /usr/lib/systemd/system/wott-agent-self-update.timer
+systemctl daemon-reload
